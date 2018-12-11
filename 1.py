ef641d146a9b417dad483347931bd2b3cc3b9817
root <ysc961126@gmail.com>
2018-12-11 17:17:32 +0000

    1

--- 1.py
+++ 1.py
@@ -1,5 +1,6 @@
 import csv
 import cv2
+import numpy as np
 from keras.models import Sequential
 from keras.layers import Flatten, Dense
 
@@ -8,27 +9,32 @@ with open('data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
+        #print(line[3])
 
+        
 images = []
 measurements = []
 for line in lines:
     source_path = line[0]
     filename = source_path.split('/')[-1]
     current_path = 'data/IMG' + filename
+    print(current_path)
     image = cv2.imread(current_path)
     images.append(image)
+    #print(line[3])
     measurement = float(line[3])
     measurements.append(measurement)
     
 X_train = np.array(images)
 y_train = np.array(measurements)
+cv2.imwrite('1.png',images[0])
 
 model = Sequential()
 model.add(Flatten(input_shape=(160,320,3)))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True)
 
 model.save('model.h5')
 
